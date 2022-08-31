44a1626676bfe670011e0b5fedbcb08b99de9f9f
wousikchoi16 <86645088+wousikchoi16@users.noreply.github.com>
2022-08-31 22:55:32 +0900

    Update psp.py

--- models/psp.py
+++ models/psp.py
@@ -98,6 +98,7 @@ class pSp(nn.Module):
         self.opts = opts
 
     def __load_latent_avg(self, ckpt, repeat=None):
+        print("ckpt['latent_avg']:", ckpt['latent_avg'])
         if 'latent_avg' in ckpt:
             self.latent_avg = ckpt['latent_avg'].to(self.opts.device)
             if repeat is not None:
